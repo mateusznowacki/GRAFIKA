17c5f09e5e922afa264dafe3a9784fd4ddb2446c
matty <mattynovy@outlook.com>
2025-01-18 15:16:56 +0100

    działające jajko 2 swaitla sterowanie

--- main.py
+++ main.py
@@ -14,13 +14,42 @@ light_ambient = [0.1, 0.1, 0.0, 1.0]
 light_diffuse = [0.8, 0.8, 0.0, 1.0]
 light_specular = [1.0, 1.0, 1.0, 1.0]
 light_position = [0.0, 0.0, 10.0, 1.0]
-
+mouse_handler = MouseEventHandler()
 att_constant = 1.0
 att_linear = 0.05
 att_quadratic = 0.001
 
 yellow_light_enabled = True
 blue_light_enabled = True
+
+mat_ambient = [0.3, 0.3, 0.3, 1.0]  # Bardziej widoczne ambient
+mat_diffuse = [1.0, 1.0, 1.0, 1.0]  # Odbijane światło
+mat_specular = [1.0, 1.0, 1.0, 1.0]  # Lśnienie
+mat_shininess = 80.0  # Intensywniejsze odbicia
+
+
+# Jasność światła czerw
+light_ambient = [0.2, 0.0, 0.0, 1.0]  # Czerwona barwa ambient
+light_diffuse = [1.0, 0.2, 0.2, 1.0]  # Intensywna czerwień
+light_specular = [1.0, 0.4, 0.4, 1.0]  # Lśniąca czerwień
+
+blue_light_ambient = [0.0, 0.0, 0.6, 1.0]  # Intensywniejsze ambient
+blue_light_diffuse = [0.0, 0.0, 1.0, 1.0]  # Głębokie, mocno niebieskie światło
+blue_light_specular = [0.5, 0.5, 1.0, 1.0]  # Subtelne lśnienie
+
+
+# Pozycja początkowa światła czerw
+mouse_handler.yellow_light_theta = 0.0
+mouse_handler.yellow_light_phi = math.pi / 4
+
+# Pozycja początkowa światła niebieskiego (po przeciwnej stronie jajka)
+mouse_handler.blue_light_theta = math.pi  # 180 stopni
+mouse_handler.blue_light_phi = math.pi / 4
+
+att_constant = 1.0
+att_linear = 0.1
+att_quadratic = 0.01
+
 
 
 egg_points = generate_egg_points(50)
@@ -45,9 +74,6 @@ def startup():
     glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, att_quadratic)
 
     # Konfiguracja światła niebieskiego
-    blue_light_ambient = [0.0, 0.0, 0.1, 1.0]
-    blue_light_diffuse = [0.0, 0.0, 1.0, 1.0]
-    blue_light_specular = [0.5, 0.5, 1.0, 1.0]
     glLightfv(GL_LIGHT1, GL_AMBIENT, blue_light_ambient)
     glLightfv(GL_LIGHT1, GL_DIFFUSE, blue_light_diffuse)
     glLightfv(GL_LIGHT1, GL_SPECULAR, blue_light_specular)
@@ -59,6 +85,7 @@ def startup():
     glEnable(GL_LIGHTING)
     glEnable(GL_LIGHT0)
     glEnable(GL_LIGHT1)
+
 
 def shutdown():
     pass
@@ -87,6 +114,15 @@ def render(time):
     gluLookAt(0.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
 
     # Aktualizacja pozycji świateł
+    if yellow_light_enabled :
+        glEnable(GL_LIGHT0)
+    else :
+        glDisable(GL_LIGHT0)
+    if blue_light_enabled :
+        glEnable(GL_LIGHT1)
+    else :
+        glDisable(GL_LIGHT1)
+
     update_light_position()
 
     # Rysowanie osi XYZ
@@ -97,26 +133,57 @@ def render(time):
 
     glFlush()
 
-
 def keyboard_key_callback(window, key, scancode, action, mods):
     global yellow_light_enabled, blue_light_enabled
 
     if action == GLFW_PRESS or action == GLFW_REPEAT:
         # Włącz/wyłącz światło żółte
         if key == GLFW_KEY_1:
-            yellow_light_enabled = not yellow_light_enabled
             if yellow_light_enabled:
-                glEnable(GL_LIGHT0)
-            else:
                 glDisable(GL_LIGHT0)
+                yellow_light_enabled = False
+                print("Światło żółte: WYŁĄCZONE")
+            else:
+                glEnable(GL_LIGHT0)
+                yellow_light_enabled = True
+                print("Światło żółte: WŁĄCZONE")
 
         # Włącz/wyłącz światło niebieskie
-        if key == GLFW_KEY_2:
-            blue_light_enabled = not blue_light_enabled
+        elif key == GLFW_KEY_2:
             if blue_light_enabled:
-                glEnable(GL_LIGHT1)
-            else:
                 glDisable(GL_LIGHT1)
+                blue_light_enabled = False
+                print("Światło niebieskie: WYŁĄCZONE")
+            else:
+                glEnable(GL_LIGHT1)
+                blue_light_enabled = True
+                print("Światło niebieskie: WŁĄCZONE")
+
+        # Sterowanie światłem żółtym wokół jajka (WASD)
+        elif key == GLFW_KEY_W:
+            mouse_handler.yellow_light_phi = max(0.0, mouse_handler.yellow_light_phi - 0.1)
+        elif key == GLFW_KEY_S:
+            mouse_handler.yellow_light_phi = min(math.pi, mouse_handler.yellow_light_phi + 0.1)
+        elif key == GLFW_KEY_A:
+            mouse_handler.yellow_light_theta -= 0.1
+        elif key == GLFW_KEY_D:
+            mouse_handler.yellow_light_theta += 0.1
+
+        # Sterowanie światłem niebieskim wokół jajka (strzałki)
+        elif key == GLFW_KEY_UP:
+            mouse_handler.blue_light_phi = max(0.0, mouse_handler.blue_light_phi - 0.1)
+        elif key == GLFW_KEY_DOWN:
+            mouse_handler.blue_light_phi = min(math.pi, mouse_handler.blue_light_phi + 0.1)
+        elif key == GLFW_KEY_LEFT:
+            mouse_handler.blue_light_theta -= 0.1
+        elif key == GLFW_KEY_RIGHT:
+            mouse_handler.blue_light_theta += 0.1
+
+        # Normalizacja kątów (zapewnia płynny ruch)
+        mouse_handler.yellow_light_theta %= 2 * math.pi
+        mouse_handler.blue_light_theta %= 2 * math.pi
+
+
 
 def update_viewport(window, width, height):
     global pix2angle
@@ -134,6 +201,8 @@ def main():
         sys.exit(-1)
 
     display_instructions()
+        # W main():
+
 
     window = glfwCreateWindow(800, 800, "Jajko 3D", None, None)
     if not window:
@@ -144,7 +213,7 @@ def main():
     glfwSetFramebufferSizeCallback(window, update_viewport)
 
     # Inicjalizacja MouseEventHandler
-    mouse_handler = MouseEventHandler()
+
     mouse_handler.register_callbacks(window)
 
     glfwSwapInterval(1)
